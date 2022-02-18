35cf02a80ba78e423841efb0c27380633154a98d
Chi Cuong Tang <chicuong.tang01@gmail.com>
2022-02-18 10:25:40 +0700

    Scaffold data from database

---
+++ BusinessObjects/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BusinessObjects.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            BillDetails = new HashSet<BillDetail>();
+        }
+
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal? Price { get; set; }
+        public int? CategoryId { get; set; }
+        public int? Stock { get; set; }
+        public bool? Status { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ICollection<BillDetail> BillDetails { get; set; }
+    }
+}
---
+++ BusinessObjects/Models/staff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BusinessObjects.Models
+{
+    public partial class staff
+    {
+        public staff()
+        {
+            Bills = new HashSet<Bill>();
+            Notifications = new HashSet<Notification>();
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string AvatarUrl { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public bool? Status { get; set; }
+
+        public virtual ICollection<Bill> Bills { get; set; }
+        public virtual ICollection<Notification> Notifications { get; set; }
+    }
+}
