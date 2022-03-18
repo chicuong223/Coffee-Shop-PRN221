64b7c852662f6915e8a3faee58a64323313bb490
Chi Cuong Tang <chicuong.tang01@gmail.com>
2022-03-18 21:36:39 +0700

    Staff Management

--- WebApp/Pages/Staff/Delete.cshtml.cs
+++ WebApp/Pages/Staff/Delete.cshtml.cs
@@ -6,14 +6,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DataObject.Models;
+using DataAccess.RepositoryInterface;
+using Microsoft.AspNetCore.Http;
 
 namespace WebApp.Pages.Staff
 {
     public class DeleteModel : PageModel
     {
-        private readonly DataObject.Models.CoffeeShopDBContext _context;
+        private readonly IRepoWrapper _context;
 
-        public DeleteModel(DataObject.Models.CoffeeShopDBContext context)
+        public DeleteModel(IRepoWrapper context)
         {
             _context = context;
         }
@@ -23,12 +25,24 @@ namespace WebApp.Pages.Staff
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
+            ISession session = HttpContext.Session;
+            var username = session.GetString("Username");
+            var role = session.GetString("Role");
+            if (username == null || role == null)
+            {
+                return RedirectToPage("../Authenticate/Login");
+            }
+            if(!role.Equals("Admin"))
+            {
+                return RedirectToPage("../Error");
+            }
+           
             if (id == null)
             {
                 return NotFound();
             }
 
-            Staff = await _context.Staff.FirstOrDefaultAsync(m => m.Username == id);
+            Staff = await _context.Staffs.GetByID(id, false);
 
             if (Staff == null)
             {
@@ -39,17 +53,30 @@ namespace WebApp.Pages.Staff
 
         public async Task<IActionResult> OnPostAsync(string id)
         {
+            ISession session = HttpContext.Session;
+            var username = session.GetString("Username");
+            var role = session.GetString("Role");
+            if (username == null || role == null)
+            {
+                return RedirectToPage("../Authenticate/Login");
+            }
+            if (!role.Equals("Admin"))
+            {
+                return RedirectToPage("../Error");
+            }
             if (id == null)
             {
                 return NotFound();
             }
 
-            Staff = await _context.Staff.FindAsync(id);
+            Staff = await _context.Staffs.GetByID(id, false);
 
             if (Staff != null)
             {
-                _context.Staff.Remove(Staff);
-                await _context.SaveChangesAsync();
+                //_context.Staff.Remove(Staff);
+                //await _context.SaveChangesAsync();
+                Staff.Status = false;
+                await _context.Staffs.Update(Staff);
             }
 
             return RedirectToPage("./Index");
--- WebApp/Pages/Staff/Edit.cshtml.cs
+++ WebApp/Pages/Staff/Edit.cshtml.cs
@@ -7,16 +7,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DataObject.Models;
+using DataAccess.RepositoryInterface;
+using Microsoft.AspNetCore.Http;
+using WebApp.Utilities;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WebApp.Pages.Staff
 {
     public class EditModel : PageModel
     {
-        private readonly DataObject.Models.CoffeeShopDBContext _context;
+        private readonly IRepoWrapper _context;
+        private readonly IWebHostEnvironment _env;
 
-        public EditModel(DataObject.Models.CoffeeShopDBContext context)
+        public EditModel(IRepoWrapper context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
 
         [BindProperty]
@@ -24,53 +30,101 @@ namespace WebApp.Pages.Staff
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
+            ISession session = HttpContext.Session;
+            var username = session.GetString("Username");
+            var role = session.GetString("Role");
+            if (username == null || role == null)
+            {
+                return RedirectToPage("../Authenticate/Login");
+            }
+
             if (id == null)
             {
                 return NotFound();
             }
 
-            Staff = await _context.Staff.FirstOrDefaultAsync(m => m.Username == id);
+            Staff = await _context.Staffs.GetByID(id, false);
 
             if (Staff == null)
             {
                 return NotFound();
             }
+
+            bool allowed = false;
+           
+
+            //Check if user is admin
+            if (role.Equals("Admin"))
+            {
+                allowed = true;
+            }
+            else
+            {
+                if (id.Equals(username))
+                {
+                    allowed = true;
+                }
+            }
+
+            if (!allowed)
+            {
+                return RedirectToAction("../Error");
+            }
+
             return Page();
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(IFormFile avatar)
         {
+            bool allowed = false;
+            ISession session = HttpContext.Session;
+            var username = session.GetString("Username");
+            var role = session.GetString("Role");
+            if(username == null || role == null)
+            {
+                return RedirectToPage("../Authenticate/Login");
+            }
+
+            //Check if user is admin
+            if(role.Equals("Admin"))
+            {
+                allowed = true;
+            }
+            else
+            {
+                if(Staff.Username.Equals(username))
+                {
+                    allowed = true;
+                }
+            }
+
+            if(!allowed)
+            {
+                return RedirectToAction("../Error");
+            }
+
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
-            _context.Attach(Staff).State = EntityState.Modified;
-
             try
             {
-                await _context.SaveChangesAsync();
+                if(avatar != null)
+                {
+                    await FileUtility.UploadFile(avatar, _env);
+                    Staff.AvatarUrl = avatar.FileName;
+                }
+                await _context.Staffs.Update(Staff);
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!StaffExists(Staff.Username))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                throw;
             }
-
             return RedirectToPage("./Index");
         }
 
-        private bool StaffExists(string id)
-        {
-            return _context.Staff.Any(e => e.Username == id);
-        }
     }
 }
